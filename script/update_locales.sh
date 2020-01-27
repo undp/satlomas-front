6f240d3e22169b6722c40b18e3bec3d35063e1a6
Damián Silvani <munshkr@gmail.com>
2020-01-27 14:44:27 -0300

    Update i18n sheet key

--- script/update_locales.sh
+++ script/update_locales.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-I18N_ID=1-gYnVIAIgUpjQO7vlXkkZs2FiM1jUj6sSyYaK6d8L8E
-#wget "https://docs.google.com/spreadsheets/d/${I18N_ID}/export?format=csv&id=${I18N_ID}&gid=0" -O i18n.csv
+I18N_ID=1WMKf6iBm7iMDqOcDUkrXMm2KaMGazK9Z7HTITJ4TR9I
 wget "https://docs.google.com/spreadsheets/d/${I18N_ID}/export?format=csv" -O i18n.csv
 script/gen_locales.py
 git add i18n.csv static/locales
