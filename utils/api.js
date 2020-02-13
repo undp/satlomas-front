80863114b03bfdbfab6941f7db897b2f81e1cacb
Damián Silvani <munshkr@gmail.com>
2020-02-13 15:55:58 -0300

    Set placeholder for host pairs

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,6 @@
 const KNOWN_HOST_PAIRS = [
   ["localhost", "localhost:8000"],
-  ["staging.app.dymaxionlabs.com", "staging.api.dymaxionlabs.com"],
-  ["app.dymaxionlabs.com", "api.dymaxionlabs.com"]
+  //["app.geolomas.com", "api.geolomas.com"]
 ];
 
 export function buildApiUrl(path) {
