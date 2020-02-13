3ce24d0e9a516c9b733410464085ce23302277f9
Damián Silvani <munshkr@gmail.com>
2020-02-13 16:02:13 -0300

    Clean HomeContent

--- components/home/HomeContent.js
+++ components/home/HomeContent.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { withStyles } from "@material-ui/core/styles";
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Typography from "@material-ui/core/Typography";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import { withNamespaces } from "../../i18n";
 import cookie from "js-cookie";
@@ -17,7 +17,7 @@ const styles = theme => ({
   root: {
     width: "100%",
     overflowX: "auto",
-    padding: "20px",
+    padding: "20px"
   },
   table: {
     minWidth: 700
@@ -27,12 +27,10 @@ const styles = theme => ({
   },
   paragraph: {
     marginBottom: "20px"
-  },
+  }
 });
 
-
 class HomeContent extends React.Component {
-
   render() {
     const { t, classes } = this.props;
     const projectId = cookie.get("project");
@@ -40,45 +38,13 @@ class HomeContent extends React.Component {
     return (
       <div>
         <Paper className={classes.root}>
-            <Typography variant="h5" component="h3" className={classes.title}>
-                {t(`home.models_title`)}
-            </Typography>
-            <Typography component="p" className={classes.paragraph}>
-                {t(`home.models_descrip`)} <a href="/models">link</a>.
-            </Typography>
-            <Typography variant="h5" component="h3" className={classes.title}>
-                {t(`home.api_title`)}
-            </Typography>
-            <Typography component="p" className={classes.paragraph}>
-                {t(`home.api_descrip1`)} <a href="/home/keys">link</a> {t(`home.api_descrip2`)}
-            </Typography>
-            <Typography variant="h5" component="h3" className={classes.title}>
-                {t(`home.project_title`)}
-            </Typography>
-            <Typography component="p" className={classes.paragraph}>
-            {t(`home.project_descrip`)} <br/>
-                <TextField
-                    id="projectId"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    defaultValue={projectId}
-                    InputProps={{
-                        readOnly: true,
-                        endAdornment: (
-                            <InputAdornment position="end">
-                            <IconButton
-                                edge="end"
-                            >
-                                <CopyToClipboard text={projectId}>
-                                    <FileCopyIcon />
-                                </CopyToClipboard>
-                            </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-            </Typography>
+          <Typography variant="h5" component="h3" className={classes.title}>
+            {t(`home.api_title`)}
+          </Typography>
+          <Typography component="p" className={classes.paragraph}>
+            {t(`home.api_descrip1`)} <a href="/home/keys">link</a>{" "}
+            {t(`home.api_descrip2`)}
+          </Typography>
         </Paper>
       </div>
     );
