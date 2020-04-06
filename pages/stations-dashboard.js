86edd748f1e7554a2b8dc06ea65ca1c6fa0e0fe0
Damián Silvani <munshkr@gmail.com>
2020-04-06 17:00:14 -0300

    New StationsDashboard page

---
+++ pages/stations-dashboard.js
@@ -0,0 +1,151 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import AppBar from "@material-ui/core/AppBar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { LineChart, XAxis, YAxis, CartesianGrid, Line } from "recharts";
+
+const styles = (theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(1500 + theme.spacing.unit * 3 * 2)]: {
+      width: 1500,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 3}px 0`,
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6,
+  },
+  body: {
+    fontFamily: "Roboto, sans-serif",
+    fontSize: 10,
+  },
+});
+
+const axisStyle = {
+  fontSize: 12,
+  fontWeight: 500,
+  fontFamily: "Roboto, sans-serif",
+};
+
+const plots = [
+  {
+    key: "temperature",
+    title: "Temperatura de Ambiente",
+  },
+  {
+    key: "humidity",
+    title: "Humedad Relativa",
+  },
+  {
+    key: "wind_speed",
+    title: "Velocidad del Viento",
+  },
+  {
+    key: "wind_direction",
+    title: "Dirección del Viento",
+  },
+  {
+    key: "pressure",
+    title: "Presión Atmosférica",
+  },
+  {
+    key: "precipitation",
+    title: "Precipitaciones (niebla)",
+  },
+  {
+    key: "pm25",
+    title: "Material Particulado (PM2.5)",
+  },
+];
+
+class StationsDashboard extends React.Component {
+  state = {
+    data: {
+      temperature: [
+        { ts: "2020-04-01", uv: 400, pv: 2400 },
+        { ts: "2020-04-02", uv: 200, pv: 1400 },
+        { ts: "2020-04-03", uv: 100, pv: 1200 },
+        { ts: "2020-04-04", uv: 600, pv: 1700 },
+        { ts: "2020-04-05", uv: 800, pv: 3400 },
+        { ts: "2020-04-06", uv: 480, pv: 4400 },
+      ],
+    },
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { data } = this.state;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              GeoLomas
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div className={classNames(classes.layout, classes.cardGrid)}>
+            <Grid container spacing={16}>
+              {plots.map((plot) => (
+                <Grid item key={plot.key} sm={6} md={4} lg={4}>
+                  <Card className={classes.card}>
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h6" component="h6">
+                        {plot.title}
+                      </Typography>
+                      <LineChart width={400} height={300} data={data[plot.key]}>
+                        <XAxis dataKey="ts" style={axisStyle} />
+                        <YAxis style={axisStyle} />
+                        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+                      </LineChart>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+StationsDashboard.getInitialProps = async () => ({
+  namespacesRequired: ["common"],
+});
+
+StationsDashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(StationsDashboard);
