205ddc92bf8a5eb1241b4d0e5f69ec3ca094761f
Damián Silvani <munshkr@gmail.com>
2020-04-29 19:42:03 -0300

    Fixes to _app and _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,14 @@
 import React from "react";
 import App, { Container } from "next/app";
-import { MuiThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import JssProvider from "react-jss/lib/JssProvider";
-import getPageContext from "../utils/getPageContext";
 import withGA from "../utils/ga";
 import { appWithTranslation, Router } from "../i18n";
 import i18next from "i18next";
 import NProgress from "next-nprogress/component";
 import { SnackbarProvider } from "notistack";
 
-class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  constructor() {
-    super();
-    this.pageContext = getPageContext();
-  }
-
+class CustomApp extends App {
   componentDidMount() {
     const {
       query: { lang },
@@ -36,54 +19,38 @@ class MyApp extends App {
       console.log(`Setting language to '${lang}'`);
       i18next.changeLanguage(lang);
     }
-
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
   }
 
   render() {
     const { Component, pageProps, analytics } = this.props;
 
+    React.useEffect(() => {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles) {
+        jssStyles.parentElement.removeChild(jssStyles);
+      }
+    }, []);
+
     return (
       <Container>
-        {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          {/* MuiThemeProvider makes the theme available down the React
-              tree thanks to React context. */}
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
-          >
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-
-            {/* Progress indicator for page transitioning */}
-            <NProgress />
-
-            {/* Pass pageContext to the _document though the renderPage enhancer
-                to render collected styles on server-side. */}
-
-            <SnackbarProvider maxSnack={3}>
-              <Component
-                pageContext={this.pageContext}
-                {...pageProps}
-                analytics={analytics}
-              />
-            </SnackbarProvider>
-          </MuiThemeProvider>
-        </JssProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* Progress indicator for page transitioning */}
+          <NProgress />
+          <SnackbarProvider maxSnack={3}>
+            <Component
+              {...pageProps}
+              analytics={analytics}
+            />
+          </SnackbarProvider>
+        </ThemeProvider>
       </Container>
     );
   }
 }
 
-MyApp = appWithTranslation(MyApp);
-MyApp = withGA("UA-105156301-5", Router)(MyApp);
+CustomApp = appWithTranslation(CustomApp);
+CustomApp = withGA("UA-105156301-5", Router)(CustomApp);
 
-export default MyApp;
+export default CustomApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Document, { Head, Main, NextScript } from "next/document";
-import flush from "styled-jsx/server";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import theme from "../utils/theme";
 
-class MyDocument extends Document {
+class CustomDocument extends Document {
   render() {
     const { pageContext } = this.props;
 
     return (
-      <html lang="en" dir="ltr">
+      <Html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
@@ -16,13 +16,8 @@ class MyDocument extends Document {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           />
+          <meta name="theme-color" content={theme.palette.primary.main} />
           {/* PWA primary color */}
-          <meta
-            name="theme-color"
-            content={
-              pageContext ? pageContext.theme.palette.primary.main : null
-            }
-          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
@@ -32,12 +27,14 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
 
-MyDocument.getInitialProps = (ctx) => {
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+CustomDocument.getInitialProps = async (ctx) => {
   // Resolution order
   //
   // On the server:
@@ -61,41 +58,21 @@ MyDocument.getInitialProps = (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  let pageContext;
-  const page = ctx.renderPage((Component) => {
-    const WrappedComponent = (props) => {
-      pageContext = props.pageContext;
-      return <Component {...props} />;
-    };
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
-    WrappedComponent.propTypes = {
-      pageContext: PropTypes.object.isRequired,
-    };
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
 
-    return WrappedComponent;
-  });
-
-  let css;
-  // It might be undefined, e.g. after an error.
-  if (pageContext) {
-    css = pageContext.sheetsRegistry.toString();
-  }
+  const initialProps = await Document.getInitialProps(ctx);
 
   return {
-    ...page,
-    pageContext,
+    ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: (
-      <React.Fragment>
-        <style
-          id="jss-server-side"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: css }}
-        />
-        {flush() || null}
-      </React.Fragment>
-    ),
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
   };
 };
 
-export default MyDocument;
+export default CustomDocument
