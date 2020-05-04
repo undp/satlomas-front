0600ffa9d39acca58ca16497296902a7c2a11aee
Damián Silvani <munshkr@gmail.com>
2020-05-04 00:26:18 -0300

    Fix PeriodSlider style issues

--- components/PeriodSlider.js
+++ components/PeriodSlider.js
@@ -1,15 +1,37 @@
-import { Slider, Typography } from '@material-ui/core';
+import {
+  Slider,
+  Tooltip,
+  createStyles,
+} from '@material-ui/core';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles({
+const ValueLabelComponent = (props) => {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+ValueLabelComponent.propTypes = {
+  children: PropTypes.element.isRequired,
+  open: PropTypes.bool.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles(theme => createStyles({
   root: {
-    width: 300,
+    marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+    width: 400,
   },
   title: {
     textShadow: '-1px 0 #ddd, 0 1px #ddd, 1px 0 #ddd, 0 -1px #ddd'
   },
-});
+}));
 
 const boxShadow = '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)';
 
@@ -27,13 +49,11 @@ export default function PeriodSlider({ periods, periodLabels, onChange }) {
 
   return (
     <div className={classes.root}>
-      <Typography id="discrete-slider" gutterBottom className={classes.title}>
-        Período
-      </Typography>
       <CustomSlider
         defaultValue={max}
         aria-labelledby="discrete-slider"
-        valueLabelDisplay="auto"
+        valueLabelDisplay="on"
+        ValueLabelComponent={ValueLabelComponent}
         valueLabelFormat={i => periodLabels ? `${periodLabels[i][0]} - ${periodLabels[i][1]}` : i}
         onChange={onChange}
         step={1}
