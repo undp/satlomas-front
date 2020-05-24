abafab71a07a35299cc9f0aa9837873709ca6ba0
Damián Silvani <munshkr@gmail.com>
2020-05-24 20:10:45 -0300

    Refactor VirtualizedTable into its own component

--- components/admin/AlertsTableContent.js
+++ components/admin/AlertsTableContent.js
@@ -1,166 +1,16 @@
 import React from 'react';
 import axios from "axios";
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, Paper, TableCell, TableSortLabel } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 import { buildApiUrl } from "../../utils/api";
-import { AutoSizer, Column, SortDirection, Table } from 'react-virtualized';
-import _ from 'lodash';
 import { withSnackbar } from 'notistack';
+import VirtualizedTable from '../VirtualizedTable';
 
-const styles = theme => ({
+const styles = _theme => ({
   root: {
     flexGrow: 1,
   },
-  table: {
-    fontFamily: theme.typography.fontFamily,
-  },
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-  },
-  tableRow: {
-    cursor: 'pointer',
-  },
-  tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
-  },
-  tableCell: {
-    flex: 1,
-  },
-  noClick: {
-    cursor: 'initial',
-  },
 });
-
-class MuiVirtualizedTable extends React.PureComponent {
-  getRowClassName = ({ index }) => {
-    const { classes, rowClassName, onRowClick } = this.props;
-
-    return classNames(classes.tableRow, classes.flexContainer, rowClassName, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex = null }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={classNames(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex, dataKey, sortBy, sortDirection }) => {
-    const { headerHeight, columns, classes, sort } = this.props;
-    const direction = {
-      [SortDirection.ASC]: 'asc',
-      [SortDirection.DESC]: 'desc',
-    };
-
-    const inner =
-      !columns[columnIndex].disableSort && sort != null ? (
-        <TableSortLabel active={dataKey === sortBy} direction={direction[sortDirection]}>
-          {label}
-        </TableSortLabel>
-      ) : (
-          label
-        );
-
-    return (
-      <TableCell
-        component="div"
-        className={classNames(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        {inner}
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table
-            className={classes.table}
-            height={height}
-            width={width}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ cellContentRenderer = null, className, dataKey, ...other }, index) => {
-              let renderer;
-              if (cellContentRenderer != null) {
-                renderer = cellRendererProps =>
-                  this.cellRenderer({
-                    cellData: cellContentRenderer(cellRendererProps),
-                    columnIndex: index,
-                  });
-              } else {
-                renderer = this.cellRenderer;
-              }
-
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={headerProps =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classNames(classes.flexContainer, className)}
-                  cellRenderer={renderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      cellContentRenderer: PropTypes.func,
-      dataKey: PropTypes.string.isRequired,
-      width: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowClassName: PropTypes.string,
-  rowHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
-  sort: PropTypes.func,
-};
-
-MuiVirtualizedTable.defaultProps = {
-  headerHeight: 56,
-  rowHeight: 56,
-};
-
-const WrappedVirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
 
 class AlertsTable extends React.Component {
@@ -187,24 +37,12 @@ class AlertsTable extends React.Component {
     });
   }
 
-  prepareRows(data) {
-    let rows = [];
-    for (let i = 0; i < data.length; i++) {
-      rows.push(data[i])
-    }
-    return rows
-  }
-
   async fetchData() {
     const { token } = this.props;
-    const response = await axios.get(buildApiUrl("/alerts"), { headers: { Authorization: token } });
-    console.log(response);
-    if (response.data.length > 0) {
-      this.setState({ loading: false, rows: this.prepareRows(response.data) });
-    }
-    else {
-      this.setState({ loading: false, rows: [] });
-    }
+    const response = await axios.get(buildApiUrl("/alerts/"), { headers: { Authorization: token } });
+    const rows = response.data;
+    console.log("Alerts:", rows);
+    this.setState({ loading: false, rows });
   }
 
   render() {
@@ -222,7 +60,7 @@ class AlertsTable extends React.Component {
         </Typography>
         <Paper>
           {!loading && (
-            <WrappedVirtualizedTable
+            <VirtualizedTable
               rowCount={rows.length}
               rowGetter={({ index }) => rows[index]}
               columns={columns}
