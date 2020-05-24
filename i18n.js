abedfc8578bc8c86bda2aad9a02790c460624cc8
Damián Silvani <munshkr@gmail.com>
2020-05-24 19:52:38 -0300

    Only es language

--- i18n.js
+++ i18n.js
@@ -3,5 +3,5 @@ var NextI18Next = require("next-i18next").default;
 module.exports = new NextI18Next({
   browserLanguageDetection: false,
   defaultLanguage: "es",
-  otherLanguages: ["en"]
+  otherLanguages: ["es"]
 });
