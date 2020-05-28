b4620c3384aeae28a9b1b87e06f7a04510e12284
Gessica <gessica@dymaxionlabs.com>
2020-05-28 15:53:32 -0300

    add onclick and open state to select control

--- components/SelectControl.js
+++ components/SelectControl.js
@@ -9,25 +9,44 @@ const styles = (theme) => ({
   },
 });
 
-let SelectControl = ({ classes, id, label, items, value, onChange }) => (
-  <FormControl component="fieldset" className={classes.formControl}>
-    <InputLabel htmlFor={id}>{label}</InputLabel>
-    <Select
-      value={value}
-      onChange={onChange}
-      inputProps={{
-        name: id,
-        id,
-      }}
-    >
-      {(items || []).map(({ id, name }) => (
-        <MenuItem key={id} value={id}>
-          {name}
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+class SelectControl extends React.Component {
+
+  state = {
+    menuOpen: false
+  };
+
+  handleOnClick = () => {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
+
+  render() {
+
+    const { classes, id, label, items, value, onChange } = this.props;
+    const { menuOpen } = this.state;
+
+    return (
+      <FormControl component="fieldset" className={classes.formControl}>
+        <InputLabel htmlFor={id}>{label}</InputLabel>
+        <Select
+          value={value}
+          onChange={onChange}
+          inputProps={{
+            name: id,
+            id,
+          }}
+          onClick={this.handleOnClick}
+          open={menuOpen}
+        >
+          {(items || []).map(({ id, name }) => (
+            <MenuItem key={id} value={id}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    );
+  }
+}
 
 SelectControl = withStyles(styles)(SelectControl);
 
