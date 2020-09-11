c18d5c107d815c506ec922348bd7e004e4e5c3b4
Damián Silvani <munshkr@gmail.com>
2020-09-10 21:05:03 -0300

    Update stations attributes config

--- config.js
+++ config.js
@@ -2,34 +2,47 @@ export default {
   appName: "SatLomas",
   stationParameters: [
     {
-      id: "temperature",
-      name: "Temperatura de Ambiente (°C)"
+      id: "altitude",
+      name: "Altitud (msnm)",
     },
     {
-      id: "humidity",
+      id: "ambient_temperature",
+      name: "Temperatura de Ambiente (°C)",
+    },
+    {
+      id: "atmospheric_pressure",
+      name: "Presión Atmosférica (hPa)",
+    },
+    {
+      id: "internal_temperature",
+      name: "Temperatura Interna (°C)",
+    },
+    {
+      id: "PM1_0",
+      name: "Material Particulado (PM1.0)",
+    },
+    {
+      id: "PM2_5",
+      name: "Material Particulado (PM2.5)",
+    },
+    {
+      id: "PM4_0",
+      name: "Material Particulado (PM4.0)",
+    },
+    {
+      id: "PM10_0",
+      name: "Material Particulado (PM10.0)",
+    },
+    {
+      id: "relative_humidity",
       name: "Humedad Relativa (%)",
     },
     {
-      id: "wind_speed",
-      name: "Velocidad del Viento (m/s)",
-    },
-    {
-      id: "wind_direction",
-      name: "Dirección del Viento (grados)",
-    },
-    {
-      id: "pressure",
-      name: "Presión Atmosférica (mbar)",
-    },
-    {
-      id: "precipitation",
-      name: "Precipitaciones (niebla)",
-    },
-    {
-      id: "pm25",
-      name: "Material Particulado (PM2.5)",
+      id: "tip_count",
+      name: "Conteo de Tipping Bucket",
     },
   ],
+  refreshIntervalMs: 1000 * 60,
   measurementContentTypes: [
     { id: "vi_lomas_changes", name: "Vegetación de Lomas" },
     { id: "lomas_changes", name: "Pérdida de Lomas" },
