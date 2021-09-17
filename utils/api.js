6b5446a0554a35eb6d11771675ba052d9151e8c1
Damián Silvani <munshkr@gmail.com>
2021-09-17 18:07:27 -0300

    Define env var for API url for buildApiUrl

--- utils/api.js
+++ utils/api.js
@@ -1,16 +1,5 @@
-const KNOWN_HOST_PAIRS = [
-  ["localhost", "localhost:8000"],
-  ["app.satlomas.dymaxionlabs.com", "api.satlomas.dymaxionlabs.com"],
-  ["satlomas.pe.undp.org", "api.satlomas.pe.undp.org"],
-];
-
 export function buildApiUrl(path) {
-  let apiHostname = location.host;
-  for (const [webHost, apiHost] of KNOWN_HOST_PAIRS) {
-    if (location.hostname === webHost) {
-      apiHostname = apiHost;
-      break;
-    }
-  }
-  return `${location.protocol}//${apiHostname}${path}`;
+  return `${
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/"
+  }${path}`;
 }
